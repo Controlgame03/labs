f5f7df16cada20949458b7c9726e8555316cbb4c
denis <denserg765@gmail.com>
2023-03-25 13:16:08 +0300

    code refactor, changed decimation, add extra task

--- mt/lab1/main.py
+++ mt/lab1/main.py
@@ -86,6 +86,131 @@ def calculateDifferenceModulation(componentArray, width, height, cmplabel):
     calculateEntropy(diffModulation2, width, height, 'H(d' + cmplabel + '^2) = ')
     calculateEntropy(diffModulation3, width, height, 'H(d' + cmplabel + '^3) = ')
     calculateEntropy(diffModulation4, width, height, 'H(d' + cmplabel + '^4) = ')
+def splitIntoSubframes(array, i_count, j_count):
+    h_downsampled, w_downsampled = height // 2, width // 2
+    A_downsampled = []
+    for i in range(0, height, 2):
+        for j in range(0, width, 2):
+            A_downsampled.append(array[(i + i_count) * w_downsampled + (j + j_count)])
+
+    A_restored = [[0 for j in range(width)] for i in range(height)]
+    for i in range(h_downsampled):
+        for j in range(w_downsampled):
+            i_new = i * 2 + 1
+            j_new = j * 2 + 1
+
+            A_restored[i_new][j_new] = A_downsampled[i * w_downsampled + j]
+            if j_new > 0:
+                A_restored[i_new][j_new - 1] = A_downsampled[i * w_downsampled + j]
+            if i_new > 0:
+                A_restored[i_new - 1][j_new] = A_downsampled[i * w_downsampled + j]
+            if i_new > 0 and j_new > 0:
+                A_restored[i_new - 1][j_new - 1] = A_downsampled[i * w_downsampled + j]
+    result = []
+    for sublist in A_restored:
+        for item in sublist:
+            result.append(item)
+    return result
+
+def createSubframes(yArray):
+    yFile = bytearray(fullImage[:pixel_offset])
+    j = 0
+    for i in range(pixel_offset, len(fullImage), 3):
+        yFile.extend(bytearray([int(yArray[j]), int(yArray[j]), int(yArray[j])]))
+        j += 1
+    return yFile
+def decimationEvenNumbered(array, times=1):
+    h_downsampled, w_downsampled = height, width
+    for time in range(times):
+        A_downsampled = []
+        for i in range(1, h_downsampled + 1, 2):
+            for j in range(1, w_downsampled + 1, 2):
+                A_downsampled.append(array[i * w_downsampled + j])
+        array = A_downsampled
+        h_downsampled, w_downsampled = h_downsampled // 2, w_downsampled // 2
+
+    A_restored = []
+    for time in range(times):
+        A_restored = [[0 for j in range(w_downsampled * 2)] for i in range(h_downsampled * 2)]
+        for i in range(h_downsampled):
+            for j in range(w_downsampled):
+                i_new = i * 2 + 1
+                j_new = j * 2 + 1
+
+                A_restored[i_new][j_new] = A_downsampled[i * w_downsampled + j]
+                if j_new > 0:
+                    A_restored[i_new][j_new - 1] = A_downsampled[i * w_downsampled + j]
+                if i_new > 0:
+                    A_restored[i_new - 1][j_new] = A_downsampled[i * w_downsampled + j]
+                if i_new > 0 and j_new > 0:
+                    A_restored[i_new - 1][j_new - 1] = A_downsampled[i * w_downsampled + j]
+        A_downsampled = [] # A_downsampled = A_restored
+        for sublist in A_restored:
+            for item in sublist:
+                A_downsampled.append(item)
+        h_downsampled *= 2
+        w_downsampled *= 2
+    return A_restored
+def decimationAriphmeticMean(array, times=1):
+    h_downsampled, w_downsampled = height, width
+    for time in range(times):
+        A_downsampled = []
+        for i in range(1, h_downsampled + 1, 2):
+            for j in range(1, w_downsampled + 1, 2):
+                A_downsampled.append((array[i * w_downsampled + j] + array[(i - 1) * w_downsampled + j] + array[i * w_downsampled + j - 1] + array[(i - 1) * w_downsampled + j - 1]) // 4)
+        array = A_downsampled
+        h_downsampled, w_downsampled = h_downsampled // 2, w_downsampled // 2
+
+    A_restored = []
+    for time in range(times):
+        A_restored = [[0 for j in range(w_downsampled * 2)] for i in range(h_downsampled * 2)]
+        for i in range(h_downsampled):
+            for j in range(w_downsampled):
+                i_new = i * 2 + 1
+                j_new = j * 2 + 1
+
+                A_restored[i_new][j_new] = A_downsampled[i * w_downsampled + j]
+                if j_new > 0:
+                    A_restored[i_new][j_new - 1] = A_downsampled[i * w_downsampled + j]
+                if i_new > 0:
+                    A_restored[i_new - 1][j_new] = A_downsampled[i * w_downsampled + j]
+                if i_new > 0 and j_new > 0:
+                    A_restored[i_new - 1][j_new - 1] = A_downsampled[i * w_downsampled + j]
+        A_downsampled = [] # A_downsampled = A_restored
+        for sublist in A_restored:
+            for item in sublist:
+                A_downsampled.append(item)
+        h_downsampled *= 2
+        w_downsampled *= 2
+    return A_restored
+
+def convertForDecimation(yArray, Cr_restored, Cb_restored, label):
+    yFile = bytearray(fullImage[:pixel_offset])
+    sumR = 0
+    sumG = 0
+    sumB = 0
+    sumCr = 0
+    sumCb = 0
+    for i in range(height):
+        for j in range(width):
+            g = cadr(yArray[i * width + j] - 0.714 * (Cr_restored[i][j] - 128) - 0.334 * (Cb_restored[i][j] - 128))
+            r = cadr(yArray[i * width + j] + 1.402 * (Cr_restored[i][j] - 128))
+            b = cadr(yArray[i * width + j] + 1.772 * (Cb_restored[i][j] - 128))
+            sumR = sumR + (imageR[i] - r) ** 2
+            sumG = sumG + (imageG[i] - g) ** 2
+            sumB = sumB + (imageB[i] - b) ** 2
+            sumCr = sumCr + (Cr_restored[i][j] - crArray[i * width + j]) ** 2
+            sumCb = sumCb + (Cb_restored[i][j] - cbArray[i * width + j]) ** 2
+            yFile.append(int(b))
+            yFile.append(int(g))
+            yFile.append(int(r))
+    print(label)
+    print('PSNR( Blue ) = ', 10 * math.log10((width * height * (255 ** 2)) / sumB))
+    print('PSNR( Green ) = ', 10 * math.log10((width * height * (255 ** 2)) / sumG))
+    print('PSNR( Red ) = ', 10 * math.log10((width * height * (255 ** 2)) / sumR))
+    print('PSNR( Cb ) = ', 10 * math.log10((width * height * (255 ** 2)) / sumCb))
+    print('PSNR( Cr ) = ', 10 * math.log10((width * height * (255 ** 2)) / sumCr))
+    return yFile
 
 print('r(red, green) = ', getR(imageR, imageG))
 print('r(red, blue) = ', getR(imageR, imageB))
@@ -181,201 +306,48 @@ imageB = [round(imageB[i]) for i in range(len(imageB))]
 imageB = np.array(imageB)
 
 
-fig = plt.figure()
-plt.title("y frequency")
-plt.hist(yArray, bins=255)
-plt.show()
+# createBarChart(yArray, "y frequency")
+# createBarChart(cbArray, "cb frequency")
+# createBarChart(crArray, "cr frequency")
+# createBarChart(imageR, "red frequency")
+# createBarChart(imageG, "green frequency")
+# createBarChart(imageB, "blue frequency")
 
-fig = plt.figure()
-plt.title("Cb frequency")
-plt.hist(cbArray, bins=255, range=(0,255))
-plt.show()
+Cb_restored = decimationEvenNumbered(cbArray)
+Cr_restored = decimationEvenNumbered(crArray)
 
-fig = plt.figure()
-plt.title("Cr frequency")
-plt.hist(crArray, bins=255, range=(0,255))
-plt.show()
+yFile = convertForDecimation(yArray,Cr_restored,Cb_restored, "even_numbered decimation x2")
 
-fig = plt.figure()
-plt.title("red frequency")
-plt.hist(imageR, bins=255)
-plt.show()
-
-fig = plt.figure()
-plt.title("green frequency")
-plt.hist(imageG, bins=255)
-plt.show()
-
-fig = plt.figure()
-plt.title("blue frequency")
-plt.hist(imageB, bins=255)
-plt.show()
-
-print(max(cbArray))
-
-print('--------------------------------------------')
-h_downsampled, w_downsampled = height//2, width//2
-print('Before:')
-print('height: ', height)
-print('width: ', width)
-print('After:')
-print('height: ', h_downsampled)
-print('width: ', w_downsampled)
-
-Cb_downsampled = []
-Cr_downsampled = []
-for i in range(0, height, 2):
-    for j in range(0, width, 2):
-        Cb_downsampled.append((cbArray[i * width + j] + cbArray[(i + 1) * width + j] + cbArray[i * width + j + 1] + cbArray[(i + 1) * width + j + 1]) // 4)
-        Cr_downsampled.append((crArray[i * width + j] + crArray[(i + 1) * width + j] + crArray[i * width + j + 1] + crArray[(i + 1) * width + j + 1]) // 4)
-
-Cb_restored = [[0 for j in range(width)] for i in range(height)]
-Cr_restored = [[0 for j in range(width)] for i in range(height)]
-
-
-for i in range(h_downsampled):
-    for j in range(w_downsampled):
-        i_new = i * 2
-        j_new = j * 2
-
-        Cb_restored[i_new][j_new] = Cb_downsampled[i * w_downsampled + j]
-        Cr_restored[i_new][j_new] = Cr_downsampled[i * w_downsampled + j]
-        if j_new > 0:
-            Cb_restored[i_new][j_new-1] = Cb_downsampled[i * w_downsampled + j]
-            Cr_restored[i_new][j_new-1] = Cr_downsampled[i * w_downsampled + j]
-        if i_new > 0:
-            Cb_restored[i_new-1][j_new] = Cb_downsampled[i * w_downsampled + j]
-            Cr_restored[i_new-1][j_new] = Cr_downsampled[i * w_downsampled + j]
-        if i_new > 0 and j_new > 0:
-            Cb_restored[i_new-1][j_new-1] = Cb_downsampled[i * w_downsampled + j]
-            Cr_restored[i_new-1][j_new-1] = Cr_downsampled[i * w_downsampled + j]
-
-yFile = bytearray(fullImage[:pixel_offset])
-sumR = 0
-sumG = 0
-sumB = 0 
-sumCr = 0
-sumCb = 0
-for i in range(height):
-    for j in range(width):
-        g = cadr(yArray[i * width + j] - 0.714 * (Cr_restored[i][j] - 128) - 0.334 * (Cb_restored[i][j] - 128))
-        r = cadr(yArray[i * width + j] + 1.402 * (Cr_restored[i][j] - 128))
-        b = cadr(yArray[i * width + j] + 1.772 * (Cb_restored[i][j] - 128))
-        sumR = sumR + (imageR[i] - r) ** 2
-        sumG = sumG + (imageG[i] - g) ** 2
-        sumB = sumB + (imageB[i] - b) ** 2
-        sumCr = sumCr + (Cr_restored[i][j] - crArray[i * width + j]) ** 2
-        sumCb = sumCb + (Cb_restored[i][j] - cbArray[i * width + j]) ** 2 
-        yFile.append(int(b))
-        yFile.append(int(g))
-        yFile.append(int(r))
-print('PSNR( Blue ) = ', 10 * math.log10((width  * height * (255 ** 2)) / sumB))
-print('PSNR( Green ) = ', 10 * math.log10((width  * height * (255 ** 2)) / sumG))
-print('PSNR( Red ) = ', 10 * math.log10((width  * height * (255 ** 2)) / sumR))
-print('PSNR( Cb ) = ', 10 * math.log10((width  * height * (255 ** 2)) / sumCb))
-print('PSNR( Cr ) = ', 10 * math.log10((width  * height * (255 ** 2)) / sumCr))
-imageFile = open('afterYCbCr_restored.bmp', 'wb')
+imageFile = open('after_EvenNumb_Decim_x2.bmp', 'wb')
 imageFile.write(bytes(yFile)) 
 imageFile.close()
 
-print('--------------------------------------------')
-h_downsampled, w_downsampled = height//2, width//2
-print('Before:')
-print('height: ', height)
-print('width: ', width)
-print('After:')
-print('height: ', h_downsampled)
-print('width: ', w_downsampled)
+Cb_restored = decimationAriphmeticMean(cbArray)
+Cr_restored = decimationAriphmeticMean(crArray)
+
+yFile = convertForDecimation(yArray,Cr_restored,Cb_restored, "ariphmetic mean decimation x2")
+
+imageFile = open('after_AriphmMean_Decim_x2.bmp', 'wb')
+imageFile.write(bytes(yFile))
+imageFile.close()
 
 
-Cb_downsampled = []
-Cr_downsampled = []
-for i in range(0, height, 2):
-    for j in range(0, width, 2):
-        Cb_downsampled.append((cbArray[i * width + j] + cbArray[(i + 1) * width + j] + cbArray[i * width + j + 1] + cbArray[(i + 1) * width + j + 1]) // 4)
-        Cr_downsampled.append((crArray[i * width + j] + crArray[(i + 1) * width + j] + crArray[i * width + j + 1] + crArray[(i + 1) * width + j + 1]) // 4)
+Cb_restored2 = decimationEvenNumbered(cbArray, times=2)
+Cr_restored2 = decimationEvenNumbered(crArray, times=2)
 
-h_downsampled2, w_downsampled2 = h_downsampled//2, w_downsampled//2
-print('After 2:')
-print('height: ', h_downsampled2)
-print('width: ', w_downsampled2)
+yFile = convertForDecimation(yArray, Cr_restored2, Cb_restored2, "even_numbered decimation x4")
 
-Cb_downsampled = []
-Cr_downsampled = []
-for i in range(0, height, 2):
-    for j in range(0, width, 2):
-        Cb_downsampled.append((cbArray[i * w_downsampled + j] + cbArray[(i + 1) * w_downsampled + j] + cbArray[i * w_downsampled + j + 1] + cbArray[(i + 1) * w_downsampled + j + 1]) // 4)
-        Cr_downsampled.append((crArray[i * w_downsampled + j] + crArray[(i + 1) * w_downsampled + j] + crArray[i * w_downsampled + j + 1] + crArray[(i + 1) * w_downsampled + j + 1]) // 4)
-
-Cb_restored = [[0 for j in range(w_downsampled)] for i in range(h_downsampled)]
-Cr_restored = [[0 for j in range(w_downsampled)] for i in range(h_downsampled)]
-
-for i in range(h_downsampled2):
-    for j in range(w_downsampled2):
-        i_new = i * 2
-        j_new = j * 2
-
-        Cb_restored[i_new][j_new] = Cb_downsampled[i * w_downsampled2 + j]
-        Cr_restored[i_new][j_new] = Cr_downsampled[i * w_downsampled2 + j]
-        if j_new > 0:
-            Cb_restored[i_new][j_new-1] = Cb_downsampled[i * w_downsampled2 + j]
-            Cr_restored[i_new][j_new-1] = Cr_downsampled[i * w_downsampled2 + j]
-        if i_new > 0:
-            Cb_restored[i_new-1][j_new] = Cb_downsampled[i * w_downsampled2 + j]
-            Cr_restored[i_new-1][j_new] = Cr_downsampled[i * w_downsampled2 + j]
-        if i_new > 0 and j_new > 0:
-            Cb_restored[i_new-1][j_new-1] = Cb_downsampled[i * w_downsampled2 + j]
-            Cr_restored[i_new-1][j_new-1] = Cr_downsampled[i * w_downsampled2 + j]
-
-Cb_restored2 = [[0 for j in range(width)] for i in range(height)]
-Cr_restored2 = [[0 for j in range(width)] for i in range(height)]
-
-for i in range(h_downsampled):
-    for j in range(w_downsampled):
-        
-        i_new = i * 2
-        j_new = j * 2
-
-
-        Cb_restored2[i_new][j_new] = Cb_restored[i][j]
-        Cr_restored2[i_new][j_new] = Cr_restored[i][j]
-        
-        if j_new > 0:
-            Cb_restored2[i_new][j_new-1] = Cb_restored[i][j]
-            Cr_restored2[i_new][j_new-1] = Cr_restored[i][j]
-        if i_new > 0:
-            Cb_restored2[i_new-1][j_new] = Cb_restored[i][j]
-            Cr_restored2[i_new-1][j_new] = Cr_restored[i][j]
-        if i_new > 0 and j_new > 0:
-            Cb_restored2[i_new-1][j_new-1] = Cb_restored[i][j]
-            Cr_restored2[i_new-1][j_new-1] = Cr_restored[i][j]
-
-yFile = bytearray(fullImage[:pixel_offset])
-sumR = 0
-sumG = 0
-sumB = 0 
-sumCr = 0
-sumCb = 0
-for i in range(height):
-    for j in range(width):
-        g = cadr(yArray[i * width + j] - 0.714 * (Cr_restored2[i][j] - 128) - 0.334 * (Cb_restored2[i][j] - 128))
-        r = cadr(yArray[i * width + j] + 1.402 * (Cr_restored2[i][j] - 128))
-        b = cadr(yArray[i * width + j] + 1.772 * (Cb_restored2[i][j] - 128))
-        sumR = sumR + (imageR[i] - r) ** 2
-        sumG = sumG + (imageG[i] - g) ** 2
-        sumB = sumB + (imageB[i] - b) ** 2
-        sumCr = sumCr + (Cr_restored2[i][j] - crArray[i * width + j]) ** 2
-        sumCb = sumCb + (Cb_restored2[i][j] - cbArray[i * width + j]) ** 2 
-        yFile.append(int(b))
-        yFile.append(int(g))
-        yFile.append(int(r))
-print('PSNR( Blue ) = ', 10 * math.log10((width  * height * (255 ** 2)) / sumB))
-print('PSNR( Green ) = ', 10 * math.log10((width  * height * (255 ** 2)) / sumG))
-print('PSNR( Red ) = ', 10 * math.log10((width  * height * (255 ** 2)) / sumR))
-print('PSNR( Cb ) = ', 10 * math.log10((width  * height * (255 ** 2)) / sumCb))
-print('PSNR( Cr ) = ', 10 * math.log10((width  * height * (255 ** 2)) / sumCr))
-imageFile = open('afterYCbCr_restored_x2.bmp', 'wb')
+imageFile = open('after_EvenNumb_Decim_x4.bmp', 'wb')
 imageFile.write(bytes(yFile)) 
+imageFile.close()
+
+Cb_restored2 = decimationAriphmeticMean(cbArray, times=2)
+Cr_restored2 = decimationAriphmeticMean(crArray, times=2)
+
+yFile = convertForDecimation(yArray, Cr_restored2, Cb_restored2, "ariphmetic mean decimation x4")
+
+imageFile = open('after_AriphmMean_Decim_x4.bmp', 'wb')
+imageFile.write(bytes(yFile))
 imageFile.close()
 
 print('--------------------------------------------')
@@ -387,9 +359,34 @@ calculateEntropy(cbArray, width, height, 'H(Cb) = ')
 calculateEntropy(crArray, width, height, 'H(Cr) = ')
 
 print('--------------------------------------------')
-calculateDifferenceModulation(imageB, width, height, 'B')
-calculateDifferenceModulation(imageG, width, height, 'G')
-calculateDifferenceModulation(imageR, width, height, 'R')
-calculateDifferenceModulation(yArray, width, height, 'Y')
-calculateDifferenceModulation(cbArray, width, height, 'Cb')
-calculateDifferenceModulation(crArray, width, height, 'Cr')
+# calculateDifferenceModulation(imageB, width, height, 'B')
+# calculateDifferenceModulation(imageG, width, height, 'G')
+# calculateDifferenceModulation(imageR, width, height, 'R')
+# calculateDifferenceModulation(yArray, width, height, 'Y')
+# calculateDifferenceModulation(cbArray, width, height, 'Cb')
+# calculateDifferenceModulation(crArray, width, height, 'Cr')
+
+yArray1 = splitIntoSubframes(yArray, 0, 0)
+yFile = createSubframes(yArray1)
+imageFile = open('y_version1.bmp', 'wb')
+imageFile.write(bytes(yFile))
+imageFile.close()
+
+yArray2 = splitIntoSubframes(yArray, 0, 1)
+yFile = createSubframes(yArray2)
+imageFile = open('y_version2.bmp', 'wb')
+imageFile.write(bytes(yFile))
+imageFile.close()
+
+yArray3 = splitIntoSubframes(yArray, 1, 0)
+yFile = createSubframes(yArray3)
+imageFile = open('y_version3.bmp', 'wb')
+imageFile.write(bytes(yFile))
+imageFile.close()
+
+yArray4 = splitIntoSubframes(yArray, 1, 1)
+yFile = createSubframes(yArray4)
+imageFile = open('y_version4.bmp', 'wb')
+imageFile.write(bytes(yFile))
+imageFile.close()
+
