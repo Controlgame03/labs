4a7eb0a7d4bb7e9a72d14ebc80273856442df0b2
Egor Karnaukhov <egor.karnaukhov@truesolv.com>
2023-05-13 10:16:49 +0300

    add logs

--- networks/4_5.py
+++ networks/4_5.py
@@ -1,10 +1,10 @@
 import random
 import matplotlib.pyplot as plt
 
-p = 0.4
+p = 0.5
 wait_time = 2
 
-n_messages = 10000
+n_messages = 100
 
 n_retries = []
 totalTime = 0
@@ -13,7 +13,7 @@ for i in range(n_messages):
     while True:
         totalTime += wait_time + 1
         if random.random() > p:
-            # print('message #', i, ' - ', retries,  ' in time: ', totalTime)
+           # print('сообщение #', i, ' - ', retries,  ' время: ', totalTime)
             break
         else:
             retries += 1
@@ -30,39 +30,25 @@ print('=========================================================================
 lineDivider = '|----------------------------------|'
 
 def getStr(tempStr):
-    diff = len(lineDivider) - len(tempStr) - 1
-    for i in range(diff):
-        tempStr += ' '
-    tempStr += '|'
     return tempStr
 
 successMessages = []
 totalTime = 0
-allT = {}
 
 while len(successMessages) != n_messages:
-    totalTime += wait_time 
+     
     for i in range(wait_time):
         if random.random() > p and len(successMessages) < n_messages:
             successMessages.append(0)
-            # print(getStr('| message #' + str(len(successMessages)) + ' at time = ' + str(totalTime)))
+            print(getStr('сообщение №' + str(len(successMessages)) + ', время = ' + str(totalTime + i)))
         else:
-            id = len(successMessages)
-            allT[id] = 1 if id not in allT else allT[id] + 1
+            
+            print(getStr('ошибка №' + str(len(successMessages) + 1) + ', время = ' + str(totalTime + i) ))
             totalTime += 1
-            # print(getStr('| error #' + str(len(successMessages)) + ' at time = ' + str(totalTime) ))
             break
-    
-    # print(lineDivider)
+    totalTime += wait_time
 
 utilization = (n_messages)/ (totalTime)
 
 print(f"Коэффициент использования канала (Практ): {utilization}")
 print(f"Коэффициент использования канала (Теор): {(1 - p)/(1 + wait_time * p)}")
-
-
-plt.plot(allT.keys(), allT.values())
-plt.title('Количество ошибок от номера сообщения')
-plt.xlabel('сообщение')
-plt.ylabel('ошибки')
-plt.show()
--- networks/main.py
+++ networks/main.py
@@ -2,10 +2,10 @@ import matplotlib.pyplot as plt
 import random
 import numpy as np
 
-p = 0.9
-wait_time = 15
+p = 0.4
+wait_time = 2
 
-n_messages = 800
+n_messages = 100
 
 successMessages = []
 virtual_channels = []
@@ -23,7 +23,7 @@ totalTime = 0
 
 c = 0
 while len(successMessages) < n_messages:
-    totalTime += wait_time + 1
+    
     for i in range(wait_time + 1):
         max_next = max(max_next, np.max(next_values))
         message = next_values.pop(0)
@@ -31,23 +31,28 @@ while len(successMessages) < n_messages:
         temp_p = random.random()
         if message < n_messages:
             c += 1
+        status = 'success |'
         if temp_p > p and message < n_messages and len(virtual_channels[i]) < max_size:
            virtual_channels[i].append(message)
-        
            next_values.append(max_next + 1)
         else:
+            status = 'error'
             next_values.append(message)
+        logStr =  't = ' + str(totalTime + i) + ' channel = ' + str(i) + ' buffer: ' + str(virtual_channels[i]) + ' | status ' + status
         
         if len(virtual_channels[i]) != 0 and virtual_channels[i][0] == next_value:
             successMessages.append(virtual_channels[i].pop(0))
+            logStr += ' value = ' + str(next_value)
             next_value += 1
+        print(logStr)
         # print(virtual_channels[i])
-    print(next_values)
-    print('-----------------')
+    totalTime += wait_time + 1
+    # print(next_values)
+    # print('-----------------')
 
 print(successMessages[-1])
 print(totalTime)
-utilization = (c)/ (totalTime)
+utilization = (n_messages) / totalTime
 
 print(f"Коэффициент использования канала (Практ): {utilization}")
 print(f"Коэффициент использования канала (Теор): {(1 - (p * max_size) / (wait_time + 1))}")
