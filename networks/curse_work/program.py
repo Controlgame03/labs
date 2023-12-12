a6c53d688fb7d83bf26524c23f946e2e750240d8
denis <denserg765@gmail.com>
2023-12-12 11:06:40 +0300

    ./networks add curse work

--- networks/curse_work/program.py
+++ networks/curse_work/program.py
@@ -10,8 +10,12 @@ input_lambda = 0.0 # не менять. начальная лямда
 M = 2 # количество пользователей
 w_min = 2 # минимальная ширина окна
 w_max = 128 #максимальная ширина окна
-queue_size = 10 # можно поставить численное значение
+queue_size = None # можно поставить численное значение
 precision = 10 # точность построения графиков
+
+success_delay = 1
+conf_delay = 0.3
+empt_delay = 0.3
 
 
 def poisson_stream(input_lambda): # сколько сообщений появилось в новом окне
@@ -22,25 +26,20 @@ def poisson_stream(input_lambda): # сколько сообщений появи
         messages_count += 1
         exp += math.exp(-input_lambda) * (input_lambda**messages_count) / math.factorial(messages_count)
     return messages_count
-
-# def message_will_be_sent_aloha(probability, window_width=None):
-#     if window_width != None:
-#         return True
-#     else:
-#         if random.random() > (1 - probability):
-#             return True
-#         return False
     
 def message_will_be_sent_interval(current_window, stats):
     if stats['window_number'] == current_window or stats['window_number'] == 0:
         return True 
     return False
 
-def model(subscriber_quantity, input_lambda, queue_size=None):
+def model(subscriber_quantity, input_lambda, queue_size, conflict_delay, empty_delay):
+    if input_lambda == 0.8:
+        pass
     subscribers_delay = [] # хранятся сообщения, которые в очереди 
     send_messages = [] # хранятся задержки отправленных сообщений
     subscriber_stats = [] # хранится ширина окна и номер выбранного окна абонента
     message_amount = []
+    total_delay = 0
     for _ in range(subscriber_quantity):
         subscribers_delay.append([])
         send_messages.append([])
@@ -60,9 +59,13 @@ def model(subscriber_quantity, input_lambda, queue_size=None):
                 current_queue.append(subscriber)
         wishing_send_amount.append(len(current_queue))
         
+        delay = 0
+
         if wishing_send_amount[-1] == 0: # пусто
+            delay = empty_delay
             pass
         elif wishing_send_amount[-1] == 1: # успех
+            delay = success_delay
             messager_number = current_queue[0]
             send_messages[messager_number].append(subscribers_delay[messager_number][0])
             subscribers_delay[messager_number].pop(0)
@@ -70,11 +73,15 @@ def model(subscriber_quantity, input_lambda, queue_size=None):
             if len(subscribers_delay[messager_number]) != 0:
                 subscriber_stats[messager_number]['window_number'] = current_window + random.SystemRandom().randint(1, subscriber_stats[messager_number]['width'])
         else: # конфликт
+            delay = conflict_delay
             for subscriber in current_queue:
                 subscriber_stats[subscriber]['width'] = min(subscriber_stats[subscriber]['width'] * 2, w_max)
                 subscriber_stats[subscriber]['window_number'] = current_window + random.SystemRandom().randint(1, subscriber_stats[subscriber]['width'])
-
-        messages_count = poisson_stream(input_lambda)
+        
+        total_delay += delay
+        messages_count = 0
+        if delay != 0:
+            messages_count = poisson_stream(input_lambda*delay)
         
         for _ in range(messages_count):
             subscriber_number = random.SystemRandom().randint(0, subscriber_quantity - 1)
@@ -85,7 +92,7 @@ def model(subscriber_quantity, input_lambda, queue_size=None):
         
         for subscriber in subscribers_delay:
             for message in range(len(subscriber)):
-                subscriber[message] += 1
+                subscriber[message] += delay
     
     average_delay = 0 # сумма всех чисел из send_messages поделенная на len(send_mes)
     output_mu = 0 # len(send_mes)/количество окон
@@ -96,26 +103,28 @@ def model(subscriber_quantity, input_lambda, queue_size=None):
         average_delay = 0
     else:
         average_delay = average_delay / output_mu
-    output_mu = output_mu / window_quantity
+    output_mu = output_mu / total_delay
         
     # average_subscribers = 0 # среднее из wishing_send_amount
     # average_subscribers = sum(wishing_send_amount) / len(wishing_send_amount)
 
     average_message_amount = sum(message_amount) / len(message_amount)
     
-    return [average_delay, average_message_amount, output_mu]
+    return [average_delay, average_message_amount, output_mu, total_delay]
     # return [average_delay, average_subscribers, output_mu]
 
 average_delays = []
 average_subscribers = []
 output_mus = []
 arguments = []
+total_delays = []
 
 for _ in range(int(1.0 * precision)):
-    result = model(M, input_lambda, queue_size)
+    result = model(M, input_lambda, queue_size, conf_delay, empt_delay)
     average_delays.append(result[0])
     average_subscribers.append(result[1])
     output_mus.append(result[2])
+    total_delays.append(result[3])
     arguments.append(input_lambda)
     input_lambda += 1 / precision
 print("lambda = " + str(input_lambda))
@@ -123,13 +132,24 @@ print("average_delays = " + str(average_delays))
 print("average_subscribers = " + str(average_subscribers))
 print("output_mus = " + str(output_mus))
 
-# average_delays_md1 =  [0, 1.0573203013642842, 1.1249752426223014, 1.218532334202621, 1.3329090228378884, 1.493138610092754, 1.7612452139170967, 2.137140385031365, 3.0031765562614567, 5.614859028592944]
-# average_subscribers_md1 =  [0, 0.10385, 0.2272, 0.36913, 0.53403, 0.74535, 1.05804, 1.48202, 2.39193, 5.07037]
-# output_mus_md1 =  [0, 0.09822, 0.20196, 0.30293, 0.40065, 0.49917, 0.6007, 0.69345, 0.79646, 0.90302]
+average_delays_basic = []
+average_subscribers_basic = []
+output_mus_basic = []
+total_delays_basic = []
+input_lambda = 0
+
+for _ in range(int(1.0 * precision)):
+    result = model(M, input_lambda, queue_size, 1, 1)
+    average_delays_basic.append(result[0])
+    average_subscribers_basic.append(result[1])
+    output_mus_basic.append(result[2])
+    total_delays_basic.append(result[3])
+    input_lambda += 1 / precision
+
 
 info = "M=" + str(M) + ",QSIZE=" + str(queue_size) + " (W_MIN=" + str(w_min) + ",W_MAX=" + str(w_max) + ")"
-plt.plot(arguments, average_delays, label='ДЭО')
-# plt.plot(arguments, average_delays_md1, label='MD1')
+plt.plot(arguments, average_delays, label='УДЭО')
+plt.plot(arguments, average_delays_basic, label='ДЭО')
 plt.legend()
 plt.title('Средняя задержка. ' + info)
 plt.xlabel('lambda')
@@ -137,19 +157,26 @@ plt.ylabel('d(lambda)')
 plt.show()
 
 
-plt.plot(arguments, average_subscribers, label='ДЭО')
-# plt.plot(arguments, average_subscribers_md1, label='MD1')
+plt.plot(arguments, average_subscribers, label='УДЭО')
+plt.plot(arguments, average_subscribers_basic, label='ДЭО')
 plt.legend()
 plt.title('Среднее количество сообщений. ' + info)
 plt.xlabel('lambda')
 plt.ylabel('N')
 plt.show()
 
-plt.plot(arguments, output_mus, label='ДЭО')
-# plt.plot(arguments, output_mus_md1, label='MD1')
+plt.plot(arguments, output_mus, label='УДЭО')
+plt.plot(arguments, output_mus_basic, label='ДЭО')
 plt.legend()
 plt.title('Интенсивность выходного потока. ' + info)
 plt.xlabel('lambda')
 plt.ylabel('mu')
 plt.show()
 
+plt.plot(arguments, total_delays, label='УДЭО')
+plt.plot(arguments, total_delays_basic, label='ДЭО')
+plt.legend()
+plt.title('Общее время работы канала. ' + info)
+plt.xlabel('lambda')
+plt.ylabel('time')
+plt.show()
--- networks/lab3/main/tmp.py
+++
@@ -1,6 +0,0 @@
-import random
-
-for _ in range(10):
-    # print(random.randint(1, 50))
-    print(random.SystemRandom().randint(1, 50))
-
